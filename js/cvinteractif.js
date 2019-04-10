5dc2c27974e5d1272be805311d28e1048ef514fa
scalap91 <clad91130@hotmail.fr>
2019-04-10 12:13:24 +0200

    new cv

---
+++ js/cvinteractif.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $("#parcours").click(function () {
+    $(".parcours").slideToggle();
+ });
+ $("#competences").click(function () {
+   $(".competences").slideToggle();
+ });
+  $("#portfolio").click(function () {
+    $(".portfolio").slideToggle();
+ });
+});
